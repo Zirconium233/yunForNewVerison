bcd13da8268e8c16e4d3637160f1f7f5fc29c72a
SINO4097 <sinoces4097@gmail.com>
2024-10-15 13:27:56 +0800

    Requirement添加计算距离所需的math依赖库 drift.py移动到tools中

--- tools/drift.py
+++ tools/drift.py
@@ -2,7 +2,34 @@ import json
 import math
 import random
 import os
-import pace_changer
+def change_pace(path, target_speed:float):
+    print("当前处理：" + path)
+    print("目标配速：" + str(target_speed))
+    tasklist_ = dict()
+    with open(file=path, mode="r+", encoding="utf-8") as f:
+        data = f.read()
+        tasklist_ = json.loads(data)
+    tasklist = tasklist_['data']
+    print("当前配速：" + str(tasklist['recodePace']))
+    factor = target_speed / tasklist['recodePace']
+    print("时间放缩因子：" + str(factor))
+    if factor > 1:
+        print("已经超越目标时间，停止放缩。")
+        return
+    tasklist['recodePace'] = round(target_speed, 2)
+    tasklist['duration'] = int(factor * tasklist['duration']) # 放缩时间
+    tasklist['recodeCadence'] = int(tasklist['recodeCadence'] / factor) # 增大步频
+    pointList = tasklist['pointsList']
+    for point in pointList:
+        point['speed'] = round(factor * point['speed'], 2)
+        point['runTime'] = str(int(float(point['runTime']) * factor))
+        point['runStep'] = str(int(float(point['runStep']) / factor))
+    print(f"完成，耗时变为：{tasklist['duration']}秒, 步频变成{tasklist['recodeCadence']}每分钟")
+    with open(file=path, mode="w+", encoding="utf-8") as f:
+        data = json.dumps(tasklist_)
+        print(f"write to {path}. ")
+        f.write(data)
+#代码来源：./PaceChanger.py
 
 # 加载 JSON 文件并提取经纬度
 def LoadJson(path):
@@ -37,10 +64,15 @@ def haversine_distance(lat1, lon1, lat2, lon2):
 #Chatgpt真好用啊#
 
 if __name__ == '__main__':
+    print("此脚本用于对已有的tasklist进行少量偏移、步频配速重新随机、并写回到原json中")
+    pos_choice = input("你需要修改哪一个校区tasklist？(1.翡翠湖 2.屯溪路 3.其他)")
+
+    path = "./tasks_fch" if pos_choice == "1" else "./tasks_txl" if pos_choice == "2" else "./tasks_else"
     chi = input("Input Change jsonfile id: ")
-    path = os.path.join(".", "tasks", "tasklist_" + chi + ".json")
+
+    FilePath = os.path.join(path, "tasklist_" + chi + ".json")
     drift = random.uniform(-0.000000001, 0.000000001)
-    lonData, latData = LoadJson(path)
+    lonData, latData = LoadJson(FilePath)
 
     for index in range(len(lonData)):
         lonData[index] += drift
@@ -55,7 +87,7 @@ if __name__ == '__main__':
 # 生成修改后的坐标列表
     ChangedData = [f"{lon},{lat}" for lon, lat in zip(lonData, latData)]
 
-    with open(path, 'r+', encoding='utf-8') as f:
+    with open(FilePath, 'r+', encoding='utf-8') as f:
         jsondata = json.load(f)
         for i in range(min(len(ChangedData), len(jsondata['data']['pointsList']))):
             jsondata['data']['pointsList'][i]['point'] = ChangedData[i]
@@ -64,6 +96,6 @@ if __name__ == '__main__':
         f.seek(0)
         json.dump(jsondata, f, indent=4, ensure_ascii=False)
         f.truncate()  
-    pace_changer.change_pace(path, round(random.uniform(4.5, 5.5), 2)) #使用已有的pace_changer.py对刚刚生成的新json步频进行放缩
+    change_pace(FilePath, round(random.uniform(4.5, 5.5), 2)) #使用已有的pace_changer.py对刚刚生成的新json步频进行放缩
 
     print('End of Program')
